0eaddb54bd3b0d8c21db7dab6fcd3b85230f3118
Dave01023 <98494416+Dave01023@users.noreply.github.com>
2022-04-09 19:23:23 -0400

    Edited Extractor Test class

--- src/test/java/edu/odu/cs/cs350/ExtractorTest.java
+++ src/test/java/edu/odu/cs/cs350/ExtractorTest.java
@@ -8,28 +8,21 @@ public class Extractor {
 @Test
 public void TestfindNames() {
 
-
-String str = "This is my test string.";
+String str = "This is David test string.";
 ArrayList<String>list = new 
 ArrayList<>(Arrays.asList(str.split(" ")));
 String rSt = str.replace(list.get(3), "<name>" + list.get(3));
 String finalSt = rSt.replace(list.get(4),list.get(3));
 String finalSt = rSt.replace(list.get(4), list.get(4)+"</name>");
-System.out.printIn(rSt);
-System.out.printIn(finalSt);
-
 
 }
 
 @Test
 public static String TestmarkPersonalNames(String textBlock) {
 
-String textBlock =  "Test block string to see if it works.";
-String names = textBlock.findNames();
-System.out.printIn("<PER>" + names + "</PER>");
-return names:
-
-
+String testBlock =  "This is David block test string.";
+String names = testBlock.findNames();
+System.out.printIn("<PER> David </PER>");
 
 
 }
