844f16d7f37e3b2459ff1000720e5eb2d09bdc9b
cs_khull001 <khull001@odu.edu>
2022-04-14 15:47:30 -0400

    added branch

--- src/main/java/edu/odu/cs/cs350/FeatureSet.java
+++ src/main/java/edu/odu/cs/cs350/FeatureSet.java
@@ -1,83 +1,80 @@
 package edu.odu.cs.cs350;
 
 public class FeatureSet {
-	
+
 	/**
 	 * Create arrays to hold the features
 	 */
-	
-	private static final String[] prefixes = { "Mr", "Ms", "Mrs", 
-			"Miss", "Dr", "Chief", "Brother", 
-			"Sister", "Father", "Professor", "General", 
-			"Rabbi", "Reverend"};
-	
-	private static final String[] iconic = { "Bridge", "University", 
-			"Statue", "Church", "Road"};
-	
+
+	private static final String[] prefixes = { "Mr", "Ms", "Mrs",
+			"Miss", "Dr", "Chief", "Brother",
+			"Sister", "Father", "Professor", "General",
+			"Rabbi", "Reverend" };
+
+	private static final String[] iconic = { "Bridge", "University",
+			"Statue", "Church", "Road" };
+
 	/**
 	 * Boolean to check if features are found in token
 	 * Eclipse recommended I format it this way
 	 * if there is an issue with it I will fix it
 	 */
-	
+
 	boolean theToken;
-	
-	public FeatureSet(String token)
-	{
+
+	public FeatureSet(String token) {
 		this.theToken = token != null;
 	}
-	
-	void isPrefix()
-	{
-		for (String prefix : prefixes)
-		{
-			if(this.theToken = prefix != null) 
+
+	void isPrefix() {
+		for (String prefix : prefixes) {
+			if (this.theToken = prefix != null)
 				return;
+		}
 	}
-	}
-	
-	void isIconic()
-	{
-		for (String icon : iconic)
-		{
-			if(this.theToken = icon != null) 
+
+	void isIconic() {
+		for (String icon : iconic) {
+			if (this.theToken = icon != null)
 				return;
+		}
 	}
-	}
-	
+
 	/**
 	 * This is all old code commented out as reference
+	 * 
 	 * @return the prefixes
 	 *
-	public String[] getPrefixes() {
-		return prefixes;
-	}
-
-	/**
+	 *         public String[] getPrefixes() {
+	 *         return prefixes;
+	 *         }
+	 * 
+	 *         /**
 	 * @param prefixes the prefixes to set
 	 *
-	public void setPrefixes(String[] prefixes) {
-		this.prefixes = prefixes;
-	}
-	
-	/**
+	 *                 public void setPrefixes(String[] prefixes) {
+	 *                 this.prefixes = prefixes;
+	 *                 }
+	 * 
+	 *                 /**
 	 * @return the iconic
 	 *
-	public String[] getIconic() {
-		return iconic;
-	}
-
-	/**
+	 *         public String[] getIconic() {
+	 *         return iconic;
+	 *         }
+	 * 
+	 *         /**
 	 * @param iconic the iconic to set
 	 *
-	public void setIconic(String[] iconic) {
-		this.iconic = iconic;
-	}
-
-	
-	/*void merge(){
-		
-		
-	}*/
+	 *               public void setIconic(String[] iconic) {
+	 *               this.iconic = iconic;
+	 *               }
+	 * 
+	 * 
+	 *               /*void merge(){
+	 * 
+	 * 
+	 *               }
+	 */
 
 }
--- src/test/java/edu/odu/cs/cs350/ExtractorTest.java
+++ src/test/java/edu/odu/cs/cs350/ExtractorTest.java
@@ -5,29 +5,24 @@ import java.util.*;
 
 public class Extractor {
 
-@Test
-public void TestfindNames() {
+    @Test
+    public void TestfindNames() {
 
-String str = "This is David test string.";
-ArrayList<String>list = new 
-ArrayList<>(Arrays.asList(str.split(" ")));
-String rSt = str.replace(list.get(3), "<name>" + list.get(3));
-String finalSt = rSt.replace(list.get(4),list.get(3));
-String finalSt = rSt.replace(list.get(4), list.get(4)+"</name>");
+        String str = "This is David test string.";
+        ArrayList<String> list = new ArrayList<>(Arrays.asList(str.split(" ")));
+        String rSt = str.replace(list.get(3), "<name>" + list.get(3));
+        String finalSt = rSt.replace(list.get(4), list.get(3));
+        String finalSt = rSt.replace(list.get(4), list.get(4) + "</name>");
 
-}
+    }
 
-@Test
-public static String TestmarkPersonalNames(String textBlock) {
+    @Test
+    public static String TestmarkPersonalNames(String textBlock) {
 
-String testBlock =  "This is David block test string.";
-String names = testBlock.findNames();
-System.out.printIn("<PER> David </PER>");
+        String testBlock = "This is David block test string.";
+        String names = testBlock.findNames();
+        System.out.println("<PER> David </PER>");
 
-
-}
-
-
-
+    }
 
 }
