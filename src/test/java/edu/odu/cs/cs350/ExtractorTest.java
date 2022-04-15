bc9d250e9e5967dfd21b6645981d17f048d5a94f
cs_khull001 <khull001@odu.edu>
2022-04-15 19:34:22 -0400

    made changes to build.gradle to make reports

--- src/test/java/edu/odu/cs/cs350/ExtractorTest.java
+++ src/test/java/edu/odu/cs/cs350/ExtractorTest.java
@@ -2,32 +2,31 @@ package edu.odu.cs.cs350;
 
 import java.util.Arrays;
 import java.util.*;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
-public class Extractor {
+public class ExtractorTest {
 
-@Test
-public void TestfindNames() {
+    @Test
+    public void TestfindNames() {
 
-String str = "This is David test string.";
-ArrayList<String>list = new 
-ArrayList<>(Arrays.asList(str.split(" ")));
-String rSt = str.replace(list.get(3), "<name>" + list.get(3));
-String finalSt = rSt.replace(list.get(4),list.get(3));
-String finalSt = rSt.replace(list.get(4), list.get(4)+"</name>");
+        String str = "This is David test string.";
+        ArrayList<String> list = new ArrayList<>(Arrays.asList(str.split(" ")));
+        String rSt = str.replace(list.get(3), "<name>" + list.get(3));
+        String finalSt = rSt.replace(list.get(4), list.get(3));
+        finalSt = rSt.replace(list.get(4), list.get(4) + "</name>");
 
-}
-
-@Test
-public static String TestmarkPersonalNames(String textBlock) {
-
-String testBlock =  "This is David block test string.";
-String names = testBlock.findNames();
-System.out.printIn("<PER> David </PER>");
-
-
-}
-
-
-
-
+    }
+    /*
+     * @Test
+     * public static String TestmarkPersonalNames(String textBlock) {
+     * 
+     * String testBlock = "This is David block test string.";
+     * String names = testBlock.findNames();
+     * System.out.println("<PER> David </PER>");
+     * 
+     * }
+     */
 }
